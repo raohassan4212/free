a76d9dd8b67098f933f45cc8e618ff42dde529aa
Rao Hassan <raohassan4212@gmail.com>
2020-10-01 19:42:31 +0500

    first commit

---
+++ class2/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      hello World by Rao Hassan <h1>Me</h1>
+    </div>
+  );
+}
+
+export default App;
